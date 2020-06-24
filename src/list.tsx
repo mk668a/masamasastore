73f6c9333e85b840f2aeb098c269de2eb16adfbd
mk668a <msftoaa@gmail.com>
2020-06-25 03:12:53 +0900

    画像追加

--- src/list.tsx
+++ src/list.tsx
@@ -3,48 +3,59 @@ import './list.scss';
 import Masonry from 'react-masonry-css'
 
 const items = [{
+    name: "ma1",
+    price: 100,
+    img: 2
+}, {
+    name: 2,
+    price: 100,
+    img: 1
+},
+{
+    name: 2,
+    price: 100,
+    img: 1
+},
+{
+    name: 2,
+    price: 100,
+    img: 2
+}, {
     name: 1,
     price: 100,
-    img: ""
+    img: 1
 }, {
     name: 2,
     price: 100,
-    img: ""
+    img: 1
 },
 {
     name: 2,
     price: 100,
-    img: ""
+    img: 1
 },
 {
     name: 2,
     price: 100,
-    img: ""
-}, {
-    name: 1,
-    price: 100,
-    img: ""
-}, {
-    name: 2,
-    price: 100,
-    img: ""
-},
-{
-    name: 2,
-    price: 100,
-    img: ""
-},
-{
-    name: 2,
-    price: 100,
-    img: ""
+    img: 1
 }]
 
 const Items = items.map((item, index) => {
+    const images = []
+    for (var i = 1; i <= item.img; i++) {
+        try {
+            const url = require(`../images/${item.name}-${i}.JPG`)
+            images.push(<img src={url} className="img"></img>)
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
     return (
         <div className="item" key={index}>
-            <img className="img" src=""></img>
-            <p className="name">タイトル{item.name}</p>
+            {images}
+            <p className="name">{item.name}</p>
             <p className="price">¥ {item.price}</p>
         </div>
     )
