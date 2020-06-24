a7998622bbe516af36ba87549837047ca0972825
mk668a <msftoaa@gmail.com>
2020-06-24 20:30:19 +0900

    デザイン

--- src/list.tsx
+++ src/list.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './list.scss';
+import Masonry from 'react-masonry-css'
 
 const items = [{
     name: 1,
@@ -39,19 +40,25 @@ const items = [{
     img: ""
 }]
 
-const List = () => {
+const Items = items.map((item, index) => {
     return (
-        <div className="list">
-            {items.map((item, index) => {
-                return (
-                    <div className="item" key={index}>
-                        <img className="img" src=""></img>
-                        <p className="name">{item.name}</p>
-                        <p className="price">{item.price}</p>
-                    </div>
-                )
-            })}
+        <div className="item" key={index}>
+            <img className="img" src=""></img>
+            <p className="name">タイトル{item.name}</p>
+            <p className="price">¥ {item.price}</p>
         </div>
+    )
+})
+
+const List = () => {
+
+    return (
+        <Masonry
+            breakpointCols={3}
+            className="list my-masonry-grid"
+            columnClassName="my-masonry-grid_column">
+            {Items}
+        </Masonry>
     );
 }
 
