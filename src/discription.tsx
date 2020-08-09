f01b35eb004aee7f5b30cb6d4bb83a91ef287028
mk668a <msftoaa@gmail.com>
2020-08-09 09:37:49 +0900

    スライドの矢印を変えた

--- src/discription.tsx
+++ src/discription.tsx
@@ -4,7 +4,11 @@ import './discription.scss'
 const Discription = () => {
     return (
         <div className="discription">
-            説明
+            <p>商品は手渡しになります。</p>
+            <p>商品が欲しい人は以下に連絡をお願いします。LINE知ってる人はLINEでもいいです。</p>
+            <p>Mail: <a href="mailto:msftoaa@gmail.com">msftoaa@gmail.com</a></p>
+            <p>Twitter: <a href="https://twitter.com/M4S4_masa" target="_blank">@M4S4_masa</a></p>
+            <p>Instagram: <a href="https://www.instagram.com/masam4sa/" target="_blank">@masam4sa</a></p>
         </div>
     );
 }
